3a68c69c620f4653d75359028b3c3fcc1e7ce9e9
NirmalMaharjjan <maharjanoptex@gmail.com>
2025-07-12 16:38:39 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_URL || '/TASK2',
+  base:process.env.VITE_BASE_URL || '/',
 })
 
